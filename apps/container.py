b7cec6a95e341c3e0744ac3ef7a303032e561530
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-07-03 14:41:35 -0500

    feat: organize imports and clean up whitespace in container.py

--- apps/container.py
+++ apps/container.py
@@ -1,10 +1,14 @@
+from sqlalchemy.orm import sessionmaker
+from config.db import engine
 from apps.users.container import UsersContainer
 from apps.auth.container import AuthContainer
+
 
 class Container:
     def __init__(self):
         self.users = UsersContainer()
         self.auth = AuthContainer()
+
 
 # Global instance
 container = Container()
