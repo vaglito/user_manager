f96829c15a179bb9ac9a495b32cd058d4d28948c
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-07-03 05:01:18 -0500

    refactor: restructure main.py to initialize FastAPI app and setup startup event

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
+import threading
 from fastapi import FastAPI
 from interfaces.api.routes import users
+from apps.users.infrastructure.models import Base
+from config.db import engine
+from adapters.consumers.user_create_consumer import start_consumer
 
-app = FastAPI()
+app = FastAPI(title="Hexagonal FastAPI App")
+
+@app.on_event("startup")
+async def on_startup():
+    # Crear tablas automáticamente
+    Base.metadata.create_all(bind=engine)
+
+    threading.Thread(target=start_consumer, daemon=True).start()
+    print("RabbitMQ consumer started")
+
 app.include_router(users.router)
